fc209976aab2cc169fc08e131559f52fba72e748
nikitka1488-52 <nikitasedunov85@gmail.com>
2025-01-23 23:07:48 +0800

    Update config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = '8046434849:AAEkR1Q4Ft0TYr3JItB1UNOEQkviKSMeke8'    
+API_KEY = 'neUqa5DyDrxLe8TCVQfe94jbJynmDrf9'   
+
+currencies = {
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Рубль': 'RUB',
+    'Юань': 'CNY',
+    'Белорусский рубль': 'BYN',
+    'Индийская рупия': 'INR'
+    }
